753d6eee0fe94a103b6b20d3e2a8b393a7b02f51
Matthew Slater <delameko@delameko-stone.com>
2022-04-29 11:35:15 +0100

    Update Mix.Config to Config

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
-use Mix.Config
+import Config
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
